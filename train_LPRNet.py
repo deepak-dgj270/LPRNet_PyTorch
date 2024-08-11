7539ce7efc5655a4b76a5ffca4301a0aafbf3dcc
deepak-dgj270 <79980172+deepak-dgj270@users.noreply.github.com>
2024-08-11 09:02:38 +0530

    Update train_LPRNet.py

--- train_LPRNet.py
+++ train_LPRNet.py
@@ -66,7 +66,7 @@ def get_parser():
     parser.add_argument('--momentum', default=0.9, type=float, help='momentum')
     parser.add_argument('--weight_decay', default=2e-5, type=float, help='Weight decay for SGD')
     parser.add_argument('--lr_schedule', default=[4, 8, 12, 14, 16], help='schedule for learning rate.')
-    parser.add_argument('--save_folder', default='/content/LPRNet_PyTorch/weights', help='Location to save checkpoint models')
+    parser.add_argument('--save_folder', default='/content/LPRNet_PyTorch/weights/', help='Location to save checkpoint models')
     # parser.add_argument('--pretrained_model', default='./weights/Final_LPRNet_model.pth', help='pretrained base model')
     parser.add_argument('--pretrained_model', default='', help='pretrained base model')
 
